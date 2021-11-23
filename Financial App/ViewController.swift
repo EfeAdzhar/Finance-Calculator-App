de565a709d2efd7a860cd1c7b3f9fade556a530b
EfeAdzhar <efeadzhar@gmail.com>
2021-11-23 12:28:53 +0200

    Creating Financial App

--- Financial App/ViewController.swift
+++ Financial App/ViewController.swift
@@ -8,11 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let button = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        createButton()
     }
-
-
+    func createButton() {
+        self.button.frame = CGRect(x: 0, y: 0, width: 200, height: 80)
+        self.button.center = self.view.center
+        self.button.setTitle("Hello", for: .normal)
+        self.view.addSubview(button)
+    }
+    
 }
 
