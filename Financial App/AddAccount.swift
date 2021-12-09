7f0ed3dad286b12bdcd887f7165c303dd4ef56a2
EfeAdzhar <efeadzhar@gmail.com>
2021-12-09 22:50:22 +0200

    Making Email and Password Labels Properly. But it's not working right...(

--- Financial App/AddAccount.swift
+++ Financial App/AddAccount.swift
@@ -3,19 +3,31 @@
 //  Created by Efe on 29.11.2021.
 
 import UIKit
-
+//MARK: Variables
 class AddAccount: UIViewController {
-    
+    let accountButton = UIButton()
+    let emailLabel = UILabel()
+    let passwordLabel = UILabel()
+
+    //MARK: View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Background Color of View
         self.view.backgroundColor = .systemPink
+        //Functions
         emailTextField()
         passwordTextField()
         labelEmail()
         labelPassword()
         addAccountButton()
+        //Targets
+        self.accountButton.addTarget(self, action: #selector(addAccountButtonPressed(sender :)), for: .touchUpInside)
+        //Responders
+        self.emailLabel.becomeFirstResponder()
+        self.passwordLabel.becomeFirstResponder()
     }
     
+    //MARK: Creating Text Fields
     func emailTextField() {
         let textField = UITextField()
         textField.frame = CGRect(x: 50, y: 300, width: 300, height: 30)
@@ -29,29 +41,85 @@ class AddAccount: UIViewController {
         let textField = UITextField()
         textField.frame = CGRect(x: 50, y: 400, width: 300, height: 30)
         textField.placeholder = "password"
+        textField.isSecureTextEntry = true
         textField.backgroundColor = .systemBlue
         self.view.addSubview(textField)
     }
     
+    //MARK: Creating Add Button
+    func addAccountButton() {
+        self.accountButton.frame = CGRect(x: 135, y: 500, width: 150, height: 60)
+        self.accountButton.setTitle("Add", for: .normal)
+        self.accountButton.backgroundColor = .systemBlue
+        self.view.addSubview(self.accountButton)
+    }
+    
+    //MARK: Labels
     func labelEmail() {
-        let label = UILabel()
-        label.text = "Email"
-        label.frame = CGRect(x: 50, y: 250, width: 100, height: 30)
-        self.view.addSubview(label)
+        emailLabel.text = "Email"
+        emailLabel.frame = CGRect(x: 50, y: 250, width: 100, height: 30)
+        self.view.addSubview(emailLabel)
     }
     
     func labelPassword() {
-        let label = UILabel()
-        label.text = "Password"
-        label.frame = CGRect(x: 50, y: 350, width: 100, height: 30)
-        self.view.addSubview(label)
+        passwordLabel.text = "Password"
+        passwordLabel.frame = CGRect(x: 50, y: 350, width: 100, height: 30)
+        self.view.addSubview(passwordLabel)
     }
     
-    func addAccountButton() {
-        let button = UIButton()
-        button.frame = CGRect(x: 135, y: 500, width: 150, height: 60)
-        button.setTitle("Add", for: .normal)
-        button.backgroundColor = .systemBlue
-        self.view.addSubview(button)
+    //MARK: Add Account Button Pressed.
+    @objc func addAccountButtonPressed(sender : UIButton) {
+        //Not working Properly :(
+        if ((self.emailLabel.text?.isEmpty) != nil) {
+            alertForEmail()
+            print("alertForEmail")
+        }
+        if ((self.passwordLabel.text?.isEmpty) != nil) {
+            alertForPassword()
+            print("alertForPassword")
+        }
+        if ((self.emailLabel.text?.isEmpty) != nil) && ((self.passwordLabel.text?.isEmpty) != nil) {
+            alertForBoth()
+            print("alertForBoth")
+        }
+    }
+}
+//MARK: Text Field Delegate
+extension AddAccount : UITextFieldDelegate {
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == emailLabel {
+            self.emailLabel.resignFirstResponder()
+        }
+        if textField == passwordLabel {
+            self.passwordLabel.resignFirstResponder()
+        }
+        return true
+    }
+}
+//MARK: Alerts
+extension AddAccount {
+    func alertForEmail() {
+        let alert = UIAlertController(title: "Your Email Bar is Empty!", message: "Please, type your email", preferredStyle: .alert)
+        let alertButtonOk = UIAlertAction(title: "OK", style: .default, handler: .none)
+        alert.addAction(alertButtonOk)
+        self.present(alert, animated: true, completion: .none)
+    }
+    
+    func alertForPassword() {
+        let alert = UIAlertController(title: "Your Password Bar is Empty!", message: "Please, type your password", preferredStyle: .alert)
+        let alertButtonOk = UIAlertAction(title: "OK", style: .default, handler: .none)
+        alert.addAction(alertButtonOk)
+        self.present(alert, animated: true, completion: .none)
+    }
+    
+    func alertForBoth() {
+        let alert = UIAlertController(title: "Your Email and Password Bars are Empty!", message: "Please, type your email and password", preferredStyle: .alert)
+        let alertButtonOk = UIAlertAction(title: "OK", style: .default, handler: .none)
+        alert.addAction(alertButtonOk)
+        self.present(alert, animated: true, completion: .none)
     }
 }
